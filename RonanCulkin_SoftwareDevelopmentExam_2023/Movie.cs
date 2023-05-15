9b0341f77cc901e86fd4c9433820595b37ce22e7
S00225953 <S00225953@LAB-D2036-13>
2023-05-15 16:36:32 +0100

    Add project files.

---
+++ RonanCulkin_SoftwareDevelopmentExam_2023/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RonanCulkin_SoftwareDevelopmentExam_2023
+{
+    public class Movie
+    {
+        // Variables
+
+    }
+
+    public class Booking
+    {
+        // Variables
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfTickets { get; set; }
+    }
+}
